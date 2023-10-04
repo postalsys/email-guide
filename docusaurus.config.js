f60f7baf19f813fae54a7739524dab6182d337bf
Andris Reinman <andris@reinman.eu>
2023-10-04 17:40:55 +0300

    config update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Email Sending Guide",
-  tagline: "Sending SaaS emails? It's not rocket science… until now.",
+  tagline: "Sending SaaS emails? It's not rocket science… until now",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
